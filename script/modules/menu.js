724128f61438bcd047752db5949c7eee1b64adcd
Nadezhda Vorotova <work-nadezhda-v@yandex.ru>
2023-08-11 17:50:11 +0400

    Добавить возможность закрытия меню

--- script/modules/menu.js
+++ script/modules/menu.js
@@ -1,6 +1,23 @@
 const button = document.querySelector('.header__menu-button');
 const menu = document.querySelector('.header__menu');
+const list = document.querySelector('.header__list');
 
 button.addEventListener('click', () => {
   menu.classList.toggle('header__menu_active');
+});
+
+const closeMenu = () => {
+  menu.classList.remove('header__menu_active');
+};
+
+document.addEventListener('click', ({target}) => {
+  if (!button.contains(target) && !menu.contains(target)) {
+    closeMenu();
+  }
+});
+
+list.addEventListener('click', ({target}) => {
+  if (target.matches('.header__link')) {
+    closeMenu();
+  }
 });
